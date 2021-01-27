555d0236adebbaf9bcb5a3f70bf0ee38e823df93
User <User@LAPTOP-48AVBV65>
2021-01-27 23:21:22 +0800

    Add/Remove Dice Function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,31 +15,66 @@ class MainActivity : AppCompatActivity() {
 
         // Find the Button in the layout
         val rollButton: Button = findViewById(R.id.button)
+        val addButton: Button = findViewById(R.id.button2)
+        var secondDice = false
 
-        //// Set a click listener on the button to roll the dice when the user taps the button
+        // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener {
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice(secondDice)
+        }
 
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-
-            rollDice()
+        // Set a click listener on the button to add the dice when the user taps the button
+        addButton.setOnClickListener {
+            if (!secondDice) {
+                Toast.makeText(this, "Dice Added!", Toast.LENGTH_SHORT).show()
+                secondDice = addDice()
+            } else {
+                Toast.makeText(this, "Dice Removed!", Toast.LENGTH_SHORT).show()
+                secondDice = removeDice()
+            }
         }
 
         // Do a dice roll when the app starts
-        rollDice()
+        rollDice(secondDice)
+    }
+
+    private fun addDice(): Boolean {
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        diceImage2.visibility = View.VISIBLE
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.text = "Remove"
+
+        return true
+    }
+
+    private fun removeDice(): Boolean {
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        diceImage2.visibility = View.INVISIBLE
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.text = "Add"
+
+        return false
     }
 
     // Roll the dice and update the screen with the result.
-    private fun rollDice() {
+    private fun rollDice(secondDice: Boolean) {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice.roll()
+        var diceRoll2 = 0
 
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
         val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        //Second Dice
+        if (secondDice) {
+            diceRoll2 = dice.roll()
+            //diceImage2.visibility = View.VISIBLE
+        }
 
         diceImage.contentDescription = "Not yet roll"
         diceImage2.contentDescription = "Not yet roll"
@@ -57,22 +93,26 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val drawableResource2 = when (diceRoll2) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
         // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
-        diceImage2.setImageResource(drawableResource2)
+
+        if (secondDice) {
+            val drawableResource2 = when (diceRoll2) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            diceImage2.setImageResource(drawableResource2)
+        }
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
-        diceImage2.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+
     }
 }
 
